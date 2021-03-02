e8c7dfe8789abd334bdac78c8c95b72d213c1a6b
Janio <cacaucodedev@gmail.com>
2021-03-01 21:11:04 -0300

    Adicionando css e o relógio do moveit

---
+++ src/components/Perfil.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/janio02011998.png" alt="autor" />
+            <div>
+                <strong>Jânio Carvalho</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level   " />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Perfil";
+
+import Head from 'next/head';
+
+import style from '../styles/Pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
-     
+    <div className={style.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
